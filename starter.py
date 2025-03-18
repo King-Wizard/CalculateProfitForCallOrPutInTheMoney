bfc46fe83d5b191526c68e557a5ded761c1ff839
King-Wizard <king.wizard.contact@gmail.com>
2025-03-18 05:08:21 +0100

    Create starter.py

---
+++ starter.py
@@ -0,0 +1,24 @@
+# https://tinyurl.com/y59xv8rj
+
+def calculateProfitForCallOrPutInTheMoney():
+    # Input values for the call or put option
+    strikePrice = float(input("Type the strike price:"))
+    
+    futurePrice = float(input("Type the future hoped price:"))
+
+    premiumPaidPerContract = float(input("Type the premium paid per contract:"))
+
+    numberOfContractsBought = float(input("Type the number of contracts bought:"))
+
+    # Calculate profit per contract
+    profitPerContract = abs(futurePrice - strikePrice) - premiumPaidPerContract
+
+    # Calculate total profit: profit per contract multiplied by the number of contracts bought
+    totalProfit = profitPerContract * numberOfContractsBought
+
+    return totalProfit
+
+
+while(True):
+    profit = calculateProfitForCallOrPutInTheMoney()
+    print(f"\nTotal profit: {profit}\n")
